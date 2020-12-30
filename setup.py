d68e138d695a433fb0b707d663b092a7f27f5729
Cyan Ho <cyan.ho@nextbank.com.tw>
2020-12-30 17:25:01 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="nextmock-pilagod",
+    name="nextmock",
     version="0.0.0",
     author="pilagod",
     author_email="pilagooood@gmail.com",
