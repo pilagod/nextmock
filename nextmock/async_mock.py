958c79c88910deda828726f63a07f45bbd1741d8
joannechen1223 <joannechen1223@gmail.com>
2021-01-19 23:05:18 +0800

    allow async mock to take async function as side effect

--- nextmock/async_mock.py
+++ nextmock/async_mock.py
@@ -1,7 +1,12 @@
+import inspect
+
 from .mock import Mock
 
 
 class AsyncMock(Mock):
 
     async def __call__(self, *args, **kwargs):
-        return super().__call__(*args, **kwargs)
+        result = super().__call__(*args, **kwargs)
+        if inspect.iscoroutine(result):
+            result = await result
+        return result
